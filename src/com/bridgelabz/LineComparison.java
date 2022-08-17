c17d99153d564ecd689851c7636431d51d66affc
Swapnil639 <swapnilpatil631997@gmail.com>
2022-08-17 17:18:11 +0530

    Added :Length Calculate

--- src/com/bridgelabz/LineComparison.java
+++ src/com/bridgelabz/LineComparison.java
@@ -1,7 +1,17 @@
 package com.bridgelabz;
 
+import java.util.Scanner;
+
 public class LineComparison {
     public static void main(String[] args) {
-        System.out.println("Welcome to Line Comparison Computation Program");
+        int x1,x2,y1,y2;
+        Scanner sc = new Scanner(System.in);
+        x1=sc.nextInt();
+        x2=sc.nextInt();
+        y1=sc.nextInt();
+        y2=sc.nextInt();
+
+        double length=Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+        System.out.println("Length :"+length);
     }
 }
